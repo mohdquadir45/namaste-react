3e81c0be120a70deed72ed64696d6b45e0c4fa22
Mohammed Quadir <86256383+mohdquadir45@users.noreply.github.com>
2024-08-04 19:59:06 +0530

    Count App using calss based component

--- components/UserClass.js
+++ components/UserClass.js
@@ -17,6 +17,13 @@ class UserClass extends React.Component{
         return (
             <div className="">
                 <h1>count is  : { count}</h1>
+                <button
+                    onClick={() => {
+                        this.setState({
+                            count : this.state.count + 1
+                        })
+                    }}
+                    >Increment count</button>
                 <h2>Name : {name}</h2>
                 <h3>Location : {location}</h3>
                 <h4>Location </h4>
